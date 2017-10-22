ebd3a2cf4264ec80337fa4bff28d1a6b8005f497
JerameyATyler <tylerj2@rpi.edu>
2017-10-22 09:20:35 -0400

    Updated schema

--- notes/schema.sql
+++ notes/schema.sql
@@ -3,6 +3,8 @@ drop TABLE  IF EXISTS nodes;
 create TABLE nodes (
   node UUID NOT NULL,
   cause UUID NOT NULL,
+  state INTEGER NOT NULL,
+  plays INTEGER NOT NULL,
   PRIMARY KEY (node, cause)
 );
 
@@ -17,6 +19,22 @@ create TABLE edges(
 drop TABLE  IF EXISTS  causes;
 create table causes(
   cause UUID NOT NULL,
-  hashtag text NOT NULL PRIMARY KEY
+  hashtag text NOT NULL PRIMARY KEY,
+  PRIMARY KEY (cause, hashtag)
 );
 
+drop TABLE IF EXISTS users;
+create table users(
+  node UUID NOT NULL,
+  first text NOT NULL,
+  last text NOT NULL,
+  email text NOT NULL,
+  password text NOT NULL,
+  salt text NOT NULL
+);
+
+drop TABLE IF EXISTS pot;
+create TABLE pot(
+  total INTEGER NOT NULL,
+  next_payout INTEGER NOT NULL
+);
